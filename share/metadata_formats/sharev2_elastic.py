5497ee1ecbe2403b15faf8328b292c7072df7d58
Abram Booth <abram@cos.io>
2020-11-16 11:04:20 -0500

    wip

--- share/metadata_formats/sharev2_elastic.py
+++ share/metadata_formats/sharev2_elastic.py
@@ -3,19 +3,22 @@ from share.util.graph import MutableGraph
 from .base import MetadataFormatter
 
 class ShareV2ElasticFormatter(MetadataFormatter):
-    def format(self, normalized_data):
-        mgraph = MutableGraph.from_jsonld(normalized_data.data)
+    def format(self, normalized_datum):
+        mgraph = MutableGraph.from_jsonld(normalized_datum.data)
         central_work = mgraph.get_central_node(guess=True)
 
         if central_work.concrete_type != 'abstractcreativework':
             return
 
         source_doc = {
-            'affiliations': None,
-            'contributors': None,
-            'date': None,
-            'date_created': None,  # TODO another query for first_normd?
-            'date_modified': normalized_data.created_at,  # TODO check date format
+            # relationships:
+            # 'affiliations': None,
+            # 'contributors': None,
+            #
+            # post-process:
+            #   'date': None,
+            'date_created': normalized_datum.created_at.isoformat(),  # TODO do another query to get the first normd under the same suid -- unsure how important
+            'date_modified': normalized_datum.created_at.isoformat(),
             'date_published': central_work['date_published'],
             'date_updated': central_work['date_updated'],
             'description': central_work['description'],
